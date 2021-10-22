46da7ff0e2aa06886539f4fa4ace9c9e1d44df6f
Nishil <826310+charsi@users.noreply.github.com>
2021-10-21 21:44:20 -0700

    Publish

--- publish.sh
+++ publish.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -ex
 
-go get -u github.com/hoffa/pt
+go get -u github.com/hoffa/pt@2ef8c2c7441fa755f991b809a83c66d82480ab81
 pt -base-url 'https://nishil.in' -highlight 'monokai' *.md posts/*.md blog/*.md blog/*/*.md projects/*.md
 git add -u
 git commit -m "Publish"
